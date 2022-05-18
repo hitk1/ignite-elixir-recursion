f8f8229cb07c2e50d9b350a7abd32a85339f102e
hitk1 <luispaulo.degini@gmail.com>
2022-05-17 22:07:58 -0300

    feat: solucao

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,19 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: getLength(list)
+
+  defp getLength([], 0), do: 0
+
+  defp getLength([]), do: 0
+
+  defp getLength([_h | tail]) do
+    getLength(tail, 1)
+  end
+
+  defp getLength([], counter) do
+    counter
+  end
+
+  defp getLength([hd | tl], counter) do
+    getLength(tl, counter + 1)
   end
 end
